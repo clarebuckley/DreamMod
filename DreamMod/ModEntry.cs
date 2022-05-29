cb9197afad84223720ec832e0079e90463a376dd
Clare <clarebuckley1@hotmail.com>
2022-05-29 20:58:45 +0100

    Dream event works (but is glitchy)

--- DreamMod/ModEntry.cs
+++ DreamMod/ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley.Menus;
 using StardewValley;
 using System;
 using System.Linq;
@@ -10,37 +11,78 @@ namespace DreamMod
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-
         private readonly int BuffUniqueID = 58012397;
         private string[] source;
+
 
         public ModEntry()
         {
             BuffUniqueID = 58012397;
-            source = new string[3] { "A strange dream...", "Did I dream that?", "How odd!" };
+            source = new string[5] 
+            { 
+                "A strange dream...", 
+                "Did I dream that?", 
+                "How odd!", 
+                "???", 
+                "!@#~??&*£)" 
+            };
         }
+
 
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
             IModEvents events = helper.Events;
-            events.GameLoop.DayStarted += this.OnDayStarted;
+            events.GameLoop.DayStarted += this.OnDayStarted; 
         }
 
 
-        /// <summary>The method called after a new day starts.</summary>
+
+        /// <summary> The method called after a new day starts.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
         private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            UpdateBuff();
-
+            //ideally this would be after the user goes to sleep but needs to be a special event made with NightlyEvent class
+            //spacecore has nightly event stuff
+            //need to add SpaceCore as a dependency to your mod since it will be referencing its DLL
+            this.DreamEvent(e);
         }
+
+
+        /// <summary Handles running the dream event.</summary>
+        /// <param name="e"> OnWarp events.</param>
+        private void DreamEvent(EventArgs e)
+        {
+            this.Monitor.Log("Starting dream event", LogLevel.Info);
+            //this is glitchy but works kind of
+            string[] dreamEventString = new[]
+            {
+                "grandpas_theme/6 6/farmer 9 9 2 Crobus 7 7 1/skippable/pause 500/emote Crobus 36/move Crobus 0 2 2/move Crobus 1 0 1/pause 500/speak Crobus \"Good night @...!$s#$b#Sweet dreams.$h\"/pause 500/emote Crobus 64/globalFade/viewport -1000 -1000/end dialogue Crobus \"I like to watch you sleep.$h\"",
+            };
+
+            Event dreamEvent = new(string.Join(string.Empty, dreamEventString));
+            dreamEvent.onEventFinished = () =>
+            {
+                UpdateBuff();
+                string[] wakeUpMessages = new string[3]
+                {
+                "You wake up from the coziest night's sleep...",
+                "Wow! That was the best night's sleep of your life!",
+                "You wake up with a newfound sense of inner peace"
+                };
+                Random random = new Random();
+                int randomIndex = random.Next(0, wakeUpMessages.Length);
+                Game1.activeClickableMenu = new DialogueBox(wakeUpMessages[randomIndex]);
+
+            };
+
+            Game1.currentLocation.startEvent(dreamEvent);
+        }
+
+
+
 
         /// <summary>After a dream, applies a buff to the player.</summary>
         private void UpdateBuff()
@@ -55,6 +97,5 @@ namespace DreamMod
             }
         }
 
- 
     }
 }
